78f18bd0fa40fd7959355744b2862056c0252a13
devgrigor <dev.grigor@gmail.com>
2019-02-26 15:49:27 +0400

    added redux example

--- client/src/page-parts/Header.js
+++ client/src/page-parts/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { createStore } from 'redux'
 
 class Header extends Component {
     constructor(props) {
@@ -8,8 +9,35 @@ class Header extends Component {
         }
     }
 
+    // example for reducx
+    counter(state = 0, action) {
+      switch(action.type) {
+        case 'INCREMENT':
+        return state + 1;
+
+        case 'DECREMENT':
+        return state - 1;
+
+        default:
+        return state;
+      }
+    }
+
+
+
     render() {
-        return (
+      // Redux is used for data manipulation asynchronousely and to provide single source of truth
+      let store = createStore(this.counter);
+
+      store.subscribe(() => {
+        console.log(store.getState());
+      });
+
+      store.dispatch({ type: 'INCREMENT' })
+      store.dispatch({ type: 'INCREMENT' })
+      store.dispatch({ type: 'DECREMENT' })
+
+      return (
             <header className="App-header">
           <img src={this.state.logo} className="App-logo" alt="logo" />
           <p>
